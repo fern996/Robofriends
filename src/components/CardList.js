edf1b8a6b5d296a1042715b9b9a397b490a307a9
sf0912 <shawnfernandes0912@gmail.com>
2022-11-09 12:40:54 -0500

    adding completed react based website files

---
+++ src/components/CardList.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Card from './Card'
+
+const CardList = ({robots}) => {
+	// const cardComponent = robots.map((user,i) => {
+	// 	return (<Card 
+	// 	key={i} 
+	// 	id= {robots[i].id} 
+	// 	name={robots[i].name} 
+	// 	email={robots[i].email}
+	// 	/>
+	// 	);
+	// })
+
+	return (
+		<div>
+		{/*{cardComponent}*/}
+		
+		{/*I can either put card component in its own const and call it like above 
+		or put the entire declaration in brackets like below. */}
+		{
+		robots.map((user,i) => {
+		return (<Card 
+		key={i} 
+		id= {robots[i].id} 
+		name={robots[i].name} 
+		email={robots[i].email}
+		/>
+		);
+	})
+		}
+		</div>	
+		);
+}
+
+export default CardList;
